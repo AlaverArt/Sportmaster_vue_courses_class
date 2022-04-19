f215dcc89345116b2ecbc99e9d6ab06dddec5369
AlaverArt <AlaverArt@yandex.ru>
2022-04-19 17:24:29 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,32 @@
 <template>
   <div id="app">
-    <ArticleList>
-      HelloWorld
-    </ArticleList>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import ArticleList from "./components/ArticleList";
-import CheckBox from "./components/CheckBox";
-import Vue from "vue";
-Vue.component('CheckBox',CheckBox);
-export default {
-  name: 'App',
-  components: {
-    ArticleList
-  }
-}
-</script>
-
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
